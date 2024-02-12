b97bda661bc662554db7860c8583cf3aaff67e82
Aglest2975 <113395721+Aglest2975@users.noreply.github.com>
2024-02-12 16:21:11 +0100

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,13 +1,48 @@
 #include <iostream>
-#include <string>
+
 using namespace std;
 
 int main()
 {
-int x;
-cout << "Enter an integer"<< endl;
-cin >> x;
-string str_number = "\""+to_string(x)+"\"";
-cout << x << " --> "<<str_number<< endl;
+    int x;
+    cout << "Enter number 0-9" << endl;
+    cin >> x;
+
+        switch(x){
+        case 0:
+        cout << "zero"<<endl;
+        break;
+        case 1:
+        cout << "one"<<endl;
+        break;
+        case 2:
+        cout << "two"<<endl;
+        break;
+        case 3:
+        cout << "three"<<endl;
+        break;
+        case 4:
+        cout << "four"<<endl;
+        break;
+        case 5:
+        cout << "five"<<endl;
+        break;
+        case 6:
+        cout << "six"<<endl;
+        break;
+        case 7:
+        cout << "seven"<<endl;
+        break;
+        case 8:
+        cout << "eight"<<endl;
+        break;
+        case 9:
+        cout << "nine"<<endl;
+        break;
+        default:
+            cout << "number is too high/low"<<endl;
+            break;
+        }
+
     return 0;
 }
